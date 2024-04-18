19f631642e408edbfadee02e3c465fa50c998ef9
zper3z <zp.per3z@gmail.com>
2024-04-18 14:28:12 -0600

    actuliazacio

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import os
 try:
-    from pipreqs
+    from googlesearch import search
+    import argparse
+except ImportError:
+    os.system("pip install google")
+    os.system("pip install argparse")
+    exit()
