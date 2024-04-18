d2ba57d0a3c96b2d0ed6e089ce25642622444258
lalo8115 <84875089+Alberto1595@users.noreply.github.com>
2024-04-18 14:55:36 -0700

    Nmap

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-try:
-    from googlesearch import search
-    import argparse
-except ImportError:
-    os.system("pip install google")
-    os.system("pip install argparse")
-    exit()
