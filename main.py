c4822e6b032aae57d64141dfb48d173fff2a1196
lalo8115 <84875089+Alberto1595@users.noreply.github.com>
2024-04-19 14:45:48 -0700

    Cambios

---
+++ main.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append(r"C:\Users\Dell\Pictures\PIA-CiberSeguridad\Modulos\BeatifullSoup")
+from bs_todosenlaces.py import obtener_enlaces_desde_url
+
+
+url = input("Ingresa URL")
