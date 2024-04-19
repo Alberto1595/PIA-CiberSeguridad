b9c1eca05093cc8c3241bb6a3dcac4f5781eeb83
lalo8115 <84875089+Alberto1595@users.noreply.github.com>
2024-04-19 14:31:20 -0700

    Actualización Shodan

--- Shodan/sho.py
+++ Shodan/sho.py
@@ -5,10 +5,12 @@ api_shodan = shodan.Shodan(api_key)
 
 buscar = input("Término a buscar: ")
 
+
+file = open("Resultados.txt", "w")
 resultados = api_shodan.search(buscar)
 
-print("Tipo de resultados:", type(resultados))
-print("Claves de resultados:", resultados.keys())
-print("Total de resultados:", resultados["total"])
-print("Tipo de coincidencias:", type(resultados["matches"]))
-print("Número de coincidencias:", len(resultados["matches"]))
+file.write("Tipo de resultados:", type(resultados + "\n"))
+file.write("Claves de resultados:", resultados.keys() + "\n")
+file.write("Total de resultados:", resultados["total"] + "\n")
+file.write("Tipo de coincidencias:", type(resultados["matches"] + "\n"))
+file.write("Número de coincidencias:", len(resultados["matches"] + "\n"))
