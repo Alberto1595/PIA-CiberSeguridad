f042be4340d035eff7343ca2c24a1cbbb4102bd4
lalo8115 <84875089+Alberto1595@users.noreply.github.com>
2024-04-26 14:50:28 -0700

    Cambios PDF

--- CreatePDF.py
+++ CreatePDF.py
@@ -1,8 +1,16 @@
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+import os
+import datetime
 
-def create_pdf(data):
+def create_pdf(data, title):
+    title_style = getSampleStyleSheet()["Title"]
+    title_paragraph = Paragraph(title, title_style)
+
+    folder_path = "Reportes PDF"
 
     style = TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.gray),
                         ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
@@ -12,10 +20,14 @@ def create_pdf(data):
                         ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
                         ('GRID', (0, 0), (-1, -1), 1, colors.black)])
 
-    pdf_filename = "scanning_results.pdf"
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+    pdf_filename = os.path.join(folder_path, f"Resultados_de_escaneo_{timestamp}.pdf")
     pdf = SimpleDocTemplate(pdf_filename, pagesize=letter)
     table = Table(data)
 
     table.setStyle(style)
 
-    pdf.build([table])
+    content = [title_paragraph, table]
+
+    pdf.build(content)
--- escaneo.py
+++ escaneo.py
@@ -3,13 +3,14 @@ from CreatePDF import create_pdf
 
 def scanning(begin, end, target):
     scanner = nmap.PortScanner()
-    results = []
+    results = ["Puerto","Estado"]
 
     for i in range(begin, end + 1):
         res = scanner.scan(target, str(i))
         state = res["scan"][target]["tcp"][i]["state"]
         results.append([f'Port {i}', state])
 
-    create_pdf(results)
+    title = f"Resultados del escaneo de puertos {target}"
+    create_pdf(results,title)
 
 scanning(10,30, "148.234.88.122")
